7048eaa61f32d3fbbd46ebacc6c3f28f22eb472b
kimdanhee <sgoiboy@gmail.com>
2021-12-13 10:18:00 +0900

    color source

---
+++ app/src/main/java/com/dany/composememeory/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.dany.composememeory.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val NormalTile = Color(0xFF1D1F26)
+val HintTile = Color(0xFFABB3C4)
+val WrongTile = Color(0xFFFF5D00)
+val CorrectTile = Color(0xFFB2E7F8)
+
+val GameStateInProgress = Color(0xFF03DAC5)
+val GameStateHint = Color(0xFFABB3C4)
+val GameStateWrong = Color(0xFFFF5D00)
+val GameStateCorrect = Color(0xFF36B44B)
+val GameStateClear = Color(0xFF36B44B)
